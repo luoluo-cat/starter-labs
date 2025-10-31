ff86e82e9bb4a861a703abaa10e867ea185e4906
luoluo-cat <1950700737@qq.com>
2025-10-31 21:15:26 +0800

    Add files via upload

--- c-basics/周报4/源.c
+++ c-basics/周报4/源.c
@@ -1,46 +1,47 @@
 #include<stdio.h>
+//union Un1
+//{
+//	char c[5];//5字节
+//	int i;//4字节
+//};
+//
+//union Un2
+//{
+//	short c[7];//2x7  14个字节
+//	int i;//4个字节
+//};
+//int check()
+//{
+//	int n = 0x11223344;
+//	char* pc = &n;
+//	if (*pc == 0x44)
+//		return 1;
+//	return 0;
+//}
 
-
-
-int sum_ptr_array(int* a[3], int lens[])
+union Un1
 {
-	int sum = 0;
-	for (int i = 0; i <= 3; i++)
-		for (int j = 0; j <= lens[i]; j++)
-			sum += a[i][j];
-	return sum;
-}
-
-
-
-int col_sum(int m, int n, int (*p)[4])
-{
-	int sum = 0;
-	for (int i = 0; i < m; i++)
+	struct Stu
 	{
-		sum += p[i][1];
-	}
-	return sum;
+		char name[20];
+		int age[10];
+	};
+	int i;
 
-}
+};
 
 
-
-int add(int a, int b)
+int check()
 {
-	return a + b;
-}
-int sub(int a, int b)
-{
-	return a - b;
-}
-int calc(int (*op)(int add,int sub), int a, int b )
-{
-	return op(a, b);
-}
+	union Un
+	{
+		char c;
+		int i;
+	}un;
 
-
-
+	un.i = 1;
+	return un.c;
+}
 
 
 
@@ -50,6 +51,30 @@ int calc(int (*op)(int add,int sub), int a, int b )
 int main()
 {
 
-	return 0;
+	//printf("%d\n", sizeof(union Un1));//结果8
+	//printf("%d\n", sizeof(union Un2));//16
 
+	
+	
+	/*不利用联合体判断大小端*/
+	//小端，低地址存低字节； 大端，低地址存高字节
+	
+	//返回1小
+	
+	int ret = check();
+	if (ret == 1)
+		printf("小端");
+	else
+	    printf("大端");
+
+
+	printf("%d\n", sizeof(union Un1));
+
+
+
+
+
+
+
+	return 0;
 }
